ee004cfbb014e608453cc2537e8a0848262dee54
shaqibmalik2000 <shaqib.malik@cirruslabs.io>
2024-02-05 14:57:41 -0500

    Project 4

--- Practice Projects/Practice-Project-4.py
+++ Practice Projects/Practice-Project-4.py
@@ -4,7 +4,7 @@ name = input("Type your name: ")
 print("Welcome", name, "to this adventure!")
 
 answer = input(
-    "You are on a dirt road it has coe to an end and you can go left or right. Which way would you like to go? ").lower()
+    "You are on a dirt road it has come to an end and you can go left or right. Which way would you like to go? ").lower()
 
 if answer == "left":
     answer = input("You come to a river, you can walk around it or swim across? Type to walk to walk around or swim to swim across. ")

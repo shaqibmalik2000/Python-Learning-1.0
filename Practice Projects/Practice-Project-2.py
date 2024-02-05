42f01fb8d3de15c18b7ff6e7bad7202d973c5717
shaqibmalik2000 <shaqib.malik@cirruslabs.io>
2024-02-05 13:45:16 -0500

    Project 2

--- Practice Projects/Practice-Project-2.py
+++ Practice Projects/Practice-Project-2.py
@@ -9,7 +9,7 @@ if top_of_range.isdigit():
 
     if top_of_range <= 0:
         print('Please type a number larger than 0 next time.')
-
+        quit()
 else:
     print('Please type a number next time.')
     quit()
